46cf83affc8dd723ce8efe9fedddfb1e097f81c9
abedshamia <ab.shamia96@gmail.com>
2022-03-08 03:46:51 +0200

    Added SSL to pool

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const {Pool} = require('pg');
 const {user, password, database, host, port} = require('./dbconfig');
 
-const pool = new Pool({user, password, database, host, port});
+const pool = new Pool({user, password, database, host, port, ssl: false});
 
 module.exports = {pool};
